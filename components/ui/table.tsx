9bd418fcf683cd5adc5ca639c1d2520e05393c32
Jason Duong <jason.duong252@gmail.com>
2026-04-21 10:44:11 +0700

    style(tickets): remove table shadow and align radius

--- components/ui/table.tsx
+++ components/ui/table.tsx
@@ -6,10 +6,10 @@ const Table = React.forwardRef<
   HTMLTableElement,
   React.HTMLAttributes<HTMLTableElement>
 >(({ className, ...props }, ref) => (
-  <div className="relative w-full overflow-auto">
+  <div className="relative w-full overflow-auto shadow-none">
     <table
       ref={ref}
-      className={cn("w-full caption-bottom text-sm", className)}
+      className={cn(className, "w-full caption-bottom text-sm shadow-none")}
       {...props}
     />
   </div>
